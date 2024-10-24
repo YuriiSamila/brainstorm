7852058e3797fa0aac7c703ded279c5f70894b54
Me <beerdrink44@gmail.com>
2024-10-24 14:42:46 +0200

    Added Quadratic solutions task

--- src/main/java/tasks/Main.java
+++ src/main/java/tasks/Main.java
@@ -1,11 +1,15 @@
 package tasks;
 
+import tasks.qudraticSolutions.QuadraticSolutions;
+
 import static tasks.kaprekarsConstant.KaprekarsConstant.kaprekarsConstant;
+import static tasks.qudraticSolutions.QuadraticSolutions.countSolutions;
 
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println(kaprekarsConstant(1703, 0));
+        //System.out.println(kaprekarsConstant(1703, 0));
+        System.out.println(countSolutions(1, 77, 1));
     }
 
 }
---
+++ src/main/java/tasks/qudraticSolutions/QuadraticSolutions.java
@@ -0,0 +1,16 @@
+package tasks.qudraticSolutions;
+
+//How many solutions does ax² + bx + c = 0 have?
+public class QuadraticSolutions {
+
+    public static int countSolutions(int a, int b, int c) {
+        int discriminator = (b * b) - 4 * a * c;
+        if (discriminator < 0) {
+            return 0;
+        }
+        return discriminator == 0
+                ? 1
+                : 2;
+    }
+
+}
