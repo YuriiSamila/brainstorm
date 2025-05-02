656b8136b398be17cb06b80a46a0277954905504
Me <beerdrink44@gmail.com>
2025-05-02 09:40:50 +0200

    Added MergeTwoSortedList task (second solution)

--- src/main/java/tasks/easy/MergeTwoSortedList.java
+++ src/main/java/tasks/easy/MergeTwoSortedList.java
@@ -2,6 +2,8 @@ package tasks.easy;
 
 import tasks.utils.ListNode;
 
+import java.util.ArrayList;
+
 public class MergeTwoSortedList {
 
     public static void main(String[] args) {
@@ -12,26 +14,70 @@ public class MergeTwoSortedList {
     }
 
     private static ListNode merge(ListNode list1, ListNode list2) {
+        ArrayList<ListNode> list = new ArrayList<>();
         if (list1 == null) {
             return list2;
         }
         if (list2 == null) {
             return list1;
         }
-    ListNode tempNode = list1;
-    ListNode prevNode = null;
-    while (tempNode != null || list2 != null) {
-        if (tempNode != null && list2 != null && tempNode.val <= list2.val) {
-            prevNode = tempNode;
-            tempNode = tempNode.next;
-        } else if (list2 != null){
-            ListNode newListNode = new ListNode(list2.val);
-            newListNode.next = tempNode;
-            prevNode.next = newListNode;
-            list2 = list2.next;
+        while (list1 != null || list2 != null) {
+            if (list1 != null && list2 != null) {
+                if (list1.val <= list2.val) {
+                    list.add(new ListNode(list1.val));
+                    list1 = list1.next;
+                } else {
+                    list.add(new ListNode(list2.val));
+                    list2 = list2.next;
+                }
+            } else if (list1 != null) {
+                list.add(new ListNode(list1.val));
+                list1 = list1.next;
+            } else {
+                list.add(new ListNode(list2.val));
+                list2 = list2.next;
+            }
         }
+        for (int i = 0; i < list.size() - 1; i++) {
+            list.get(i).next = list.get(i + 1);
+        }
+        return list.isEmpty()
+                ? list1
+                : list.getFirst();
     }
-    return list1;
+
+    private static ListNode merge2(ListNode list1, ListNode list2) {
+        if (list1 == null) {
+            return list2;
+        }
+        if (list2 == null) {
+            return list1;
+        }
+        ListNode tempNode;
+        ListNode tempNode2;
+        boolean firstLess = list1.val <= list2.val;
+        if (firstLess) {
+            tempNode = list1;
+            tempNode2 = list2;
+        } else {
+            tempNode = list2;
+            tempNode2 = list1;
+        }
+        ListNode prevNode = null;
+        while (tempNode != null || tempNode2 != null) {
+            if (tempNode != null && tempNode2 != null && tempNode.val <= tempNode2.val) {
+                prevNode = tempNode;
+                tempNode = tempNode.next;
+            } else if (tempNode2 != null) {
+                ListNode newListNode = new ListNode(tempNode2.val);
+                newListNode.next = tempNode;
+                prevNode.next = newListNode;
+                tempNode2 = tempNode2.next;
+            }
+        }
+        return firstLess
+                ? list1
+                : list2;
     }
 
 }
